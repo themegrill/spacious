744d3f320f3cd5c02cd3e6f9a974380be5ae4c39
Romos <romosamatya17@gmail.com>
2016-05-31 16:56:20 +0545

    View Pro button added in Free Vs Pro Table.

--- inc/admin/class-spacious-admin.php
+++ inc/admin/class-spacious-admin.php
@@ -461,6 +461,13 @@ class Spacious_Admin {
 						<td><span class="dashicons dashicons-no"></span></td>
 						<td><span class="dashicons dashicons-yes"></span></td>
 					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td class="btn-wrapper">
+							<a href="<?php echo esc_url( apply_filters( 'spacious_pro_theme_url', 'http://themegrill.com/themes/spacious-pro/' ) ); ?>" class="button button-secondary docs" target="_blank"><?php _e( 'View Pro', 'spacious' ); ?></a>
+						</td>
+					</tr>
 				</tbody>
 			</table>
 
