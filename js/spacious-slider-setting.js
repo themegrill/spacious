c1c4412f4ef65d6ce7bc8890547624d33b96ca34
rabindratharu <rabindra.tharu.np@gmail.com>
2016-06-24 11:54:06 +0545

    load issue fixed for slider.

--- js/spacious-slider-setting.js
+++ js/spacious-slider-setting.js
@@ -3,19 +3,19 @@
  *
  * Contains all the slider settings for the featured post/page slider.
  */
-jQuery(document).ready(function(){
+jQuery(window).on('load',function() {
 	jQuery( '.slider-cycle' ).cycle({
-		fx: 'fade',
-		timeout: 4000,
-		speed: 1000,
-		slides: '> div',
-		pager: '> #controllers',
-		pagerActiveClass: 'active',
-		pagerTemplate: '<a></a>',
-		pauseOnHover: true,
-		autoHeight: 'container',
-		swipe: true,
-		swipeFx: 'scrollHorz',
-		log: false
+	fx: 'fade',
+	timeout: 4000,
+	speed: 1000,
+	slides: '> div',
+	pager: '> #controllers',
+	pagerActiveClass: 'active',
+	pagerTemplate: '<a></a>',
+	pauseOnHover: true,
+	autoHeight: 'container',
+	swipe: true,
+	swipeFx: 'scrollHorz',
+	log: false
 	});
 });
