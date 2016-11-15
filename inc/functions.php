3fcb9310e2c170a3f8453dc5e862ced715095231
rabindratharu <rabindra.tharu.np@gmail.com>
2016-11-15 17:46:52 +0545

    integration of new responsive menu design.

--- inc/functions.php
+++ inc/functions.php
@@ -183,7 +183,9 @@ function spacious_body_class( $classes ) {
 	elseif( $layout_meta == 'no_sidebar_full_width' ) { $classes[] = 'no-sidebar-full-width'; }
 	elseif( $layout_meta == 'no_sidebar_content_centered' ) { $classes[] = 'no-sidebar'; }
 
-
+	if( spacious_options( 'spacious_new_menu', '' ) != '' ){
+		$classes[] = 'better-responsive-menu';
+	}
 	if ( spacious_options( 'spacious_archive_display_type', 'blog_large' ) == 'blog_medium_alternate' ) {
 		$classes[] = 'blog-alternate-medium';
 	}
--- js/navigation.js
+++ js/navigation.js
@@ -36,3 +36,11 @@
 		}
 	};
 } )();
+jQuery(document).ready(function() {
+    jQuery('.better-responsive-menu #site-navigation .menu-item-has-children').append('<span class="sub-toggle"> <i class="fa fa-caret-down"></i> </span>');
+    jQuery('.better-responsive-menu #site-navigation .sub-toggle').click(function() {
+        jQuery(this).parent('.menu-item-has-children').children('ul.sub-menu').first().slideToggle('1000');
+        jQuery(this).children('.fa-caret-right').first().toggleClass('fa-caret-down');
+        jQuery(this).toggleClass('active');
+    });
+});
