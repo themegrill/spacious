c5ef20b683634558cf749106582cb2a04b37186b
Rabin Shrestha <sun.ravi90@gmail.com>
2017-01-26 14:35:22 +0545

    Demo importer plugin link added to about page

--- inc/admin/class-spacious-admin.php
+++ inc/admin/class-spacious-admin.php
@@ -181,6 +181,11 @@ class Spacious_Admin {
 
 			<div class="changelog point-releases">
 				<div class="under-the-hood two-col">
+               <div class="col">
+                  <h3><?php esc_html_e( 'Import Demo', 'spacious' ); ?></h3>
+                  <p><?php esc_html_e( 'Needs ThemeGrill Demo Importer plugin.', 'spacious' ) ?></p>
+                  <p><a href="<?php echo esc_url( network_admin_url( 'plugin-install.php?tab=search&type=term&s=themegrill-demo-importer' ) ); ?>" class="button button-primary"><?php esc_html_e( 'Install', 'spacious' ); ?></a></p>
+               </div>
 					<div class="col">
 						<h3><?php esc_html_e( 'Theme Customizer', 'spacious' ); ?></h3>
 						<p><?php esc_html_e( 'All Theme Options are available via Customize screen.', 'spacious' ) ?></p>
