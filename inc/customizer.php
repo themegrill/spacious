6717a4efb0e8ed7f4e35b4e2658491e2ea30c724
Bishal Napit <mebishalnapit@gmail.com>
2017-11-30 11:38:31 +0545

    Add customizer live refresh for site title and description

--- inc/customizer.php
+++ inc/customizer.php
@@ -747,6 +747,17 @@ function spacious_customize_partial_blogdescription() {
 	bloginfo( 'description' );
 }
 
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ *
+ * @since Spacious 1.4.9
+ */
+function spacious_customize_preview_js() {
+	wp_enqueue_script( 'spacious-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), false, true );
+}
+
+add_action( 'customize_preview_init', 'spacious_customize_preview_js' );
+
 /*****************************************************************************************/
 
 /*
