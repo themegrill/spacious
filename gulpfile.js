204320a5c40c21bebe56e58a5446df9d8722c4be
Prbha Lama <lprabha47@gmail.com>
2018-03-07 14:41:32 +0545

    Added - style  for header And footer variations

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,10 @@ var paths = {
 		src: './inc/elementor/assets/SCSS/**/*.scss',
 		dest: './inc/elementor/assets/css/'
 	},
+	styles: {
+		src: './assets/scss/**/*.scss',
+		dest: './'
+	},
 	php: {
 		src: [ './*.php', './post-templates/*.php' ]
 	}
@@ -51,9 +55,28 @@ function elementorStylesCompile() {
 		.pipe( browserSync.stream() );
 }
 
+
+function sassCompile() {
+	return gulp.src( paths.styles.src )
+		.pipe( sass( {
+			indentType: 'tab',
+			indentWidth: 1,
+			outputStyle: 'expanded'
+		} ).on( 'error', sass.logError ) )
+		.pipe( postcss( [
+			autoprefixer( {
+				browsers: [ 'last 2 versions' ],
+				cascade: false
+			} )
+		] ) )
+		.pipe( gulp.dest( paths.styles.dest ) )
+		.pipe( browserSync.stream() );
+}
+
 // Watch for file changes
 function watch() {
 	gulp.watch( paths.elementorStyles.src, elementorStylesCompile );
+	gulp.watch( paths.styles.src, sassCompile );
 	gulp.watch( [ paths.js.src, paths.php.src ], browserSyncReload );
 }
 
@@ -64,5 +87,6 @@ var server = gulp.series( browserSyncStart, watch );
 exports.browserSyncStart = browserSyncStart;
 exports.browserSyncReload = browserSyncReload;
 exports.elementorStylesCompile = elementorStylesCompile;
+exports.sassCompile = sassCompile;
 exports.watch = watch;
 exports.server = server;
