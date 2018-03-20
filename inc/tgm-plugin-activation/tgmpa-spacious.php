65445a7ca79132ffbef9f5ac0656e7e21e58a332
Bishal Napit <mebishalnapit@gmail.com>
2018-03-20 14:27:36 +0545

    Recommend Spacious Toolkit via TGMPA

--- inc/tgm-plugin-activation/tgmpa-spacious.php
+++ inc/tgm-plugin-activation/tgmpa-spacious.php
@@ -11,6 +11,7 @@ add_action( 'tgmpa_register', 'spacious_register_required_plugins' );
  * This function is hooked into `tgmpa_register`, which is fired on the WP `init` action on priority 10.
  */
 function spacious_register_required_plugins() {
+
 	/**
 	 * Array of plugin arrays. Required keys are name and slug.
 	 */
@@ -18,64 +19,78 @@ function spacious_register_required_plugins() {
 
 		// Include ThemeGrill Demo Importer as recommended
 		array(
- 			'name'      => 'ThemeGrill Demo Importer',
-			'slug'      => 'themegrill-demo-importer',
-			'required'  => false,
+			'name'     => 'ThemeGrill Demo Importer',
+			'slug'     => 'themegrill-demo-importer',
+			'required' => false,
+		),
+
+		// Include Spacious Toolkit as recommended
+		array(
+			'name'     => 'Spacious Toolkit',
+			'slug'     => 'spacious-toolkit',
+			'required' => false,
 		),
 
 	);
 
 	$config = array(
-		'id'           => 'spacious',              // Unique ID for hashing notices for multiple instances of TGMPA.
-		'default_path' => '',                      // Default absolute path to bundled plugins.
-		'menu'         => 'tgmpa-install-plugins', // Menu slug.
-		'has_notices'  => true,                    // Show admin notices or not.
-		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
-		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
-		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
-		'message'      => '',                      // Message to output right before the plugins table.
+		'id'           => 'spacious',
+		// Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',
+		// Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins',
+		// Menu slug.
+		'has_notices'  => true,
+		// Show admin notices or not.
+		'dismissable'  => true,
+		// If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',
+		// If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,
+		// Automatically activate plugins after installation or not.
+		'message'      => '',
+		// Message to output right before the plugins table.
 
-		
-		'strings'      => array(
+		'strings' => array(
 			'page_title'                      => esc_html__( 'Install Required Plugins', 'spacious' ),
 			'menu_title'                      => esc_html__( 'Install Plugins', 'spacious' ),
-			 /* translators: %s: plugin name.*/ 
+			/* translators: %s: plugin name.*/
 			'installing'                      => esc_html__( 'Installing Plugin: %s', 'spacious' ),
 			/* translators: %s: plugin name. */
 			'updating'                        => esc_html__( 'Updating Plugin: %s', 'spacious' ),
 			'oops'                            => esc_html__( 'Something went wrong with the plugin API.', 'spacious' ),
 			'notice_can_install_required'     => _n_noop(
-				/* translators: 1: plugin name(s). */
+			/* translators: 1: plugin name(s). */
 				'This theme requires the following plugin: %1$s.',
 				'This theme requires the following plugins: %1$s.',
 				'spacious'
 			),
 			'notice_can_install_recommended'  => _n_noop(
-				/* translators: 1: plugin name(s). */
+			/* translators: 1: plugin name(s). */
 				'This theme recommends the following plugin: %1$s.',
 				'This theme recommends the following plugins: %1$s.',
 				'spacious'
 			),
 			'notice_ask_to_update'            => _n_noop(
-				/* translators: 1: plugin name(s). */
+			/* translators: 1: plugin name(s). */
 				'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.',
 				'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.',
 				'spacious'
 			),
 			'notice_ask_to_update_maybe'      => _n_noop(
-				/* translators: 1: plugin name(s). */
+			/* translators: 1: plugin name(s). */
 				'There is an update available for: %1$s.',
 				'There are updates available for the following plugins: %1$s.',
 				'spacious'
 			),
 			'notice_can_activate_required'    => _n_noop(
-				/* translators: 1: plugin name(s). */
+			/* translators: 1: plugin name(s). */
 				'The following required plugin is currently inactive: %1$s.',
 				'The following required plugins are currently inactive: %1$s.',
 				'spacious'
 			),
 			'notice_can_activate_recommended' => _n_noop(
-				/* translators: 1: plugin name(s). */
+			/* translators: 1: plugin name(s). */
 				'The following recommended plugin is currently inactive: %1$s.',
 				'The following recommended plugins are currently inactive: %1$s.',
 				'spacious'
@@ -85,7 +100,7 @@ function spacious_register_required_plugins() {
 				'Begin installing plugins',
 				'spacious'
 			),
-			'update_link' 					  => _n_noop(
+			'update_link'                     => _n_noop(
 				'Begin updating plugin',
 				'Begin updating plugins',
 				'spacious'
@@ -108,9 +123,10 @@ function spacious_register_required_plugins() {
 			'notice_cannot_install_activate'  => esc_html__( 'There are one or more required or recommended plugins to install, update or activate.', 'spacious' ),
 			'contact_admin'                   => esc_html__( 'Please contact the administrator of this site for help.', 'spacious' ),
 
-			'nag_type'                        => '', // Determines admin notice type - can only be one of the typical WP notice classes, such as 'updated', 'update-nag', 'notice-warning', 'notice-info' or 'error'. Some of which may not work as expected in older WP versions.
+			'nag_type' => '',
+			// Determines admin notice type - can only be one of the typical WP notice classes, such as 'updated', 'update-nag', 'notice-warning', 'notice-info' or 'error'. Some of which may not work as expected in older WP versions.
 		),
-		
+
 	);
 
 	tgmpa( $plugins, $config );
