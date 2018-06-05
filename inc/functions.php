09e92b925cdb2bb23c27338a21c9aa7e68de7ab0
Rajesh Poudel <razyees@gmail.com>
2018-06-05 12:54:23 +0545

    Fix - post per page value

--- inc/functions.php
+++ inc/functions.php
@@ -101,7 +101,7 @@ if ( ! function_exists( 'spacious_related_posts_function' ) ) {
 			'ignore_sticky_posts'    => 1,
 			'orderby'                => 'rand',
 			'post__not_in'           => array( $post->ID ),
-			'posts_per_page'         => spacious_options( 'spacious_related_post_number_display', '3' ),
+			'posts_per_page'         => 3,
 		);
 
 		// Related by categories.
