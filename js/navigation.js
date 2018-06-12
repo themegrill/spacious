70c56dc2eb3f4f14808dc6fc09e4a913ff758b01
Ashish Shrestha <ashishsthanp@gmail.com>
2018-06-12 16:43:45 +0545

    Fix - menu out of viewport new way

--- js/navigation.js
+++ js/navigation.js
@@ -7,16 +7,16 @@
 	var container, button, menu;
 
 	container = document.getElementById( 'site-navigation' );
-	if ( ! container ) {
+	if ( !container ) {
 		return;
 	}
 
-	button = container.getElementsByClassName( 'menu-toggle' )[0];
+	button = container.getElementsByClassName( 'menu-toggle' )[ 0 ];
 	if ( 'undefined' === typeof button ) {
 		return;
 	}
 
-	menu = container.getElementsByTagName( 'ul' )[0];
+	menu = container.getElementsByTagName( 'ul' )[ 0 ];
 
 	// Hide menu toggle button if menu is empty and return early.
 	if ( 'undefined' === typeof menu ) {
@@ -38,86 +38,88 @@
 } )();
 
 // Show Submenu on click on touch enabled deviced
-( function () {
-    var container;
-    container = document.getElementById( 'site-navigation' );
+( function() {
+	var container;
+	container = document.getElementById( 'site-navigation' );
 
-    /**
-     * Toggles `focus` class to allow submenu access on tablets.
-     */
-    ( function( container ) {
-        var touchStartFn, i,
-            parentLink = container.querySelectorAll( '.menu-item-has-children > a, .page_item_has_children > a' );
+	/**
+	 * Toggles `focus` class to allow submenu access on tablets.
+	 */
+	( function( container ) {
+		var touchStartFn, i,
+		    parentLink = container.querySelectorAll( '.menu-item-has-children > a, .page_item_has_children > a' );
 
-        if ( ( 'ontouchstart' in window ) && ( window.matchMedia( "( min-width: 768px ) " ).matches ) ) {
-            touchStartFn = function( e ) {
-                var menuItem = this.parentNode, i;
+		if ( ( 'ontouchstart' in window ) && ( window.matchMedia( "( min-width: 768px ) " ).matches ) ) {
+			touchStartFn = function( e ) {
+				var menuItem = this.parentNode, i;
 
-                if ( ! menuItem.classList.contains( 'focus' ) ) {
-                    e.preventDefault();
-                    for ( i = 0; i < menuItem.parentNode.children.length; ++i ) {
-                        if ( menuItem === menuItem.parentNode.children[i] ) {
-                            continue;
-                        }
-                        menuItem.parentNode.children[i].classList.remove( 'focus' );
-                    }
-                    menuItem.classList.add( 'focus' );
-                } else {
-                    menuItem.classList.remove( 'focus' );
-                }
-            };
-
-            for ( i = 0; i < parentLink.length; ++i ) {
-                parentLink[i].addEventListener( 'touchstart', touchStartFn, false );
-            }
-        }
-    }( container ) );
-} ) ();
-
-/**
- * Fixes menu out of viewport
- */
-( function ( $ ) {
-	var handlerIn, handlerOut,
-		container = document.getElementById( 'site-navigation' );
-
-	// For touchscreen and mouse enter
-	handlerIn = function () {
-		if ( $( this ).children( 'ul.sub-menu' ).length > 0 ) {
-
-			// Get document width
-			var docWidth = $( document ).width();
-
-			// Get window width
-			var windowWidth = $( window ).width();
-
-			// Condition where menu item goes out of viewport
-			if ( docWidth > windowWidth ) {
-				$( this ).children( ' ul.sub-menu' ).addClass( 'spacious-menu--left' );
-			}
-		}
-	};
-
-	// For mouse leave
-	handlerOut = function () {
-		$( this ).children( ' ul.sub-menu' ).removeClass( 'spacious-menu--left' );
-	};
-
-	// Desktop
-	$( '.main-navigation  .menu-item-has-children, .main-navigation .page_item_has_children' ).hover( handlerIn, handlerOut );
-
-
-	// Touch screen
-	( function ( container ) {
-		var i,
-			parentLink = container.querySelectorAll( '.main-navigation  .menu-item-has-children, .main-navigation .page_item_has_children' );
-
-		if ( 'ontouchstart' in window ) {
+				if ( !menuItem.classList.contains( 'focus' ) ) {
+					e.preventDefault();
+					for ( i = 0; i < menuItem.parentNode.children.length; ++i ) {
+						if ( menuItem === menuItem.parentNode.children[ i ] ) {
+							continue;
+						}
+						menuItem.parentNode.children[ i ].classList.remove( 'focus' );
+					}
+					menuItem.classList.add( 'focus' );
+				} else {
+					menuItem.classList.remove( 'focus' );
+				}
+			};
 
 			for ( i = 0; i < parentLink.length; ++i ) {
-				parentLink[ i ].addEventListener( 'touchstart', handlerIn, false );
+				parentLink[ i ].addEventListener( 'touchstart', touchStartFn, false );
 			}
 		}
-	} )( container );
+	}( container ) );
+} )();
 
-} )( jQuery );
+/**
+ * Fix: Menu out of view port
+ */
+( function() {
+
+	var subMenu;
+
+	jQuery( '.main-navigation  .menu-item-has-children, .main-navigation .page_item_has_children' ).on( {
+
+		'mouseover touchstart': function() {
+
+			function isElementInViewport( subMenu ) {
+
+				if ( 'function' === typeof jQuery && subMenu instanceof jQuery ) {
+					subMenu = subMenu[ 0 ];
+				}
+
+				// In case browser doesn't support getBoundingClientRect function.
+				if ( 'function' === typeof subMenu.getBoundingClientRect ) {
+
+					var rect = subMenu.getBoundingClientRect();
+
+					if ( rect.right > ( window.innerWidth || document.documentElement.clientWidth ) ) {
+						return 'spacious-menu--left'; // menu goes out of viewport from right.
+					} else if ( rect.left < 0 ) {
+						return 'spacious-menu--right'; // menu goes out of viewport from left.
+					} else {
+						return false;
+					}
+				}
+			}
+
+			subMenu = jQuery( this ).children( '.sub-menu, .children' );
+
+			// If menu item has submenu
+			if ( subMenu.length > 0 ) {
+
+				var viewportClass = isElementInViewport( subMenu );
+
+				if ( false !== viewportClass ) {
+					subMenu.addClass( viewportClass );
+				}
+			}
+
+		}
+
+	} );
+
+} )();
