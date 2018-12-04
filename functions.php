d3b338930fdbee6b18eb2b09f511ed3fa0b4bbed
Bishal Napit <mebishalnapit@gmail.com>
2018-12-04 16:38:47 +0545

    Add - New theme notice

--- functions.php
+++ functions.php
@@ -415,9 +415,12 @@ if ( class_exists( 'TG_Demo_Importer' ) ) {
 $theme            = wp_get_theme( 'spacious' );
 $spacious_version = $theme['Version'];
 
-/* Calling in the admin area for the Welcome Page */
+/**
+ * Calling in the admin area for the Welcome Page as well as for the new theme notice too.
+ */
 if ( is_admin() ) {
 	require_once get_template_directory() . '/inc/admin/class-spacious-admin.php';
+	require_once get_template_directory() . '/inc/admin/class-spacious-new-theme-notice.php';
 }
 
 /**
