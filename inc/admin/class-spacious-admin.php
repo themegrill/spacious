ccd0ac2b4b832526061c0ca0323623ba7a53a48d
NituShrestha <shresthanitu984@gmail.com>
2020-05-11 16:14:07 +0545

    Tweak - update get started notice

--- inc/admin/class-spacious-admin.php
+++ inc/admin/class-spacious-admin.php
@@ -7,191 +7,203 @@
  * @since   1.3.8
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+defined( 'ABSPATH' ) || exit;
 
-if ( ! class_exists( 'Spacious_Admin' ) ) :
+/**
+ * Class Spacious_Admin
+ */
+class Spacious_Admin {
 
 	/**
-	 * Spacious_Admin Class.
+	 * Spacious_Admin constructor.
 	 */
-	class Spacious_Admin {
+	public function __construct() {
+		add_action( 'wp_loaded', array( $this, 'admin_notice' ), 20 );
+		add_action( 'wp_loaded', array( $this, 'hide_notices' ), 15 );
+		add_action( 'wp_ajax_import_button', array( $this, 'tg_ajax_import_button_handler' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'tg_ajax_enqueue_scripts' ) );
+	}
 
-		/**
-		 * Constructor.
-		 */
-		public function __construct() {
-			add_action( 'wp_loaded', array( __CLASS__, 'hide_notices' ) );
-			add_action( 'wp_loaded', array( $this, 'admin_notice' ) );
-			add_action( 'wp_ajax_import_button', array( $this, 'spacious_ajax_import_button_handler' ) );
-			add_action( 'admin_enqueue_scripts', array( $this, 'spacious_ajax_enqueue_scripts' ) );
+	/**
+	 * Localize array for import button AJAX request.
+	 */
+	public function tg_ajax_enqueue_scripts() {
+
+		wp_enqueue_script( 'spacious-plugin-install-helper', get_template_directory_uri() . '/inc/admin/js/plugin-handle.js', array( 'jquery' ), 1, true );
+
+		$translation_array = array(
+			'uri'      => esc_url( admin_url( '/themes.php?page=demo-importer&browse=all&spacious-hide-notice=welcome' ) ),
+			'btn_text' => esc_html__( 'Processing...', 'spacious' ),
+			'nonce'    => wp_create_nonce( 'spacious_demo_import_nonce' ),
+		);
+
+		wp_localize_script( 'spacious-plugin-install-helper', 'spacious_redirect_demo_page', $translation_array );
+
+	}
+
+	/**
+	 * Handle the AJAX process while import or get started button clicked.
+	 */
+	public function tg_ajax_import_button_handler() {
+
+		check_ajax_referer( 'spacious_demo_import_nonce', 'security' );
+
+		$state = '';
+
+		if ( class_exists( 'themegrill_demo_importer' ) ) {
+			$state = 'activated';
+		} elseif ( file_exists( WP_PLUGIN_DIR . '/themegrill-demo-importer/themegrill-demo-importer.php' ) ) {
+			$state = 'installed';
 		}
 
-		/**
-		 * Localize array for import button AJAX request.
-		 */
-		public function spacious_ajax_enqueue_scripts() {
+		if ( 'activated' === $state ) {
+			$response['redirect'] = admin_url( '/themes.php?page=demo-importer&browse=all&spacious-hide-notice=welcome' );
+		} elseif ( 'installed' === $state ) {
+			$response['redirect'] = admin_url( '/themes.php?page=demo-importer&browse=all&spacious-hide-notice=welcome' );
+			if ( current_user_can( 'activate_plugin' ) ) {
+				$result = activate_plugin( 'themegrill-demo-importer/themegrill-demo-importer.php' );
 
-			wp_enqueue_script( 'updates' );
-			wp_enqueue_script( 'spacious-plugin-install-helper', get_template_directory_uri() . '/inc/admin/js/plugin-handle.js', array( 'jquery' ), 1, true );
-			wp_localize_script(
-				'spacious-plugin-install-helper', 'spacious_plugin_helper',
-				array(
-					'activating' => esc_html__( 'Activating ', 'spacious' ),
-				)
-			);
-
-			$translation_array = array(
-				'uri'      => esc_url( admin_url( '/themes.php?page=demo-importer&browse=all&spacious-hide-notice=welcome' ) ),
-				'btn_text' => esc_html__( 'Processing...', 'spacious' ),
-				'nonce'    => wp_create_nonce( 'spacious_demo_import_nonce' ),
-			);
-
-			wp_localize_script( 'spacious-plugin-install-helper', 'spacious_redirect_demo_page', $translation_array );
-
-		}
-
-		/**
-		 * Handle the AJAX process while import or get started button clicked.
-		 */
-		public function spacious_ajax_import_button_handler() {
-
-			check_ajax_referer( 'spacious_demo_import_nonce', 'security' );
-
-			$state = '';
-			if ( is_plugin_active( 'themegrill-demo-importer/themegrill-demo-importer.php' ) ) {
-				$state = 'activated';
-			} elseif ( file_exists( WP_PLUGIN_DIR . '/themegrill-demo-importer/themegrill-demo-importer.php' ) ) {
-				$state = 'installed';
-			}
-
-			if ( 'activated' === $state ) {
-				$response['redirect'] = admin_url( '/themes.php?page=demo-importer&browse=all&spacious-hide-notice=welcome' );
-			} elseif ( 'installed' === $state ) {
-				$response['redirect'] = admin_url( '/themes.php?page=demo-importer&browse=all&spacious-hide-notice=welcome' );
-				if ( current_user_can( 'activate_plugin' ) ) {
-					$result = activate_plugin( 'themegrill-demo-importer/themegrill-demo-importer.php' );
-
-					if ( is_wp_error( $result ) ) {
-						$response['errorCode']    = $result->get_error_code();
-						$response['errorMessage'] = $result->get_error_message();
-					}
+				if ( is_wp_error( $result ) ) {
+					$response['errorCode']    = $result->get_error_code();
+					$response['errorMessage'] = $result->get_error_message();
 				}
+			}
+		} else {
+			wp_enqueue_style( 'plugin-install' );
+			wp_enqueue_script( 'plugin-install' );
+			$response['redirect'] = admin_url( '/themes.php?page=demo-importer&browse=all&spacious-hide-notice=welcome' );
+			/**
+			 * Install Plugin.
+			 */
+			include_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
+			include_once ABSPATH . 'wp-admin/includes/plugin-install.php';
+
+			$api = plugins_api( 'plugin_information', array(
+				'slug'   => sanitize_key( wp_unslash( 'themegrill-demo-importer' ) ),
+				'fields' => array(
+					'sections' => false,
+				),
+			) );
+
+			$skin     = new WP_Ajax_Upgrader_Skin();
+			$upgrader = new Plugin_Upgrader( $skin );
+			$result   = $upgrader->install( $api->download_link );
+			if ( $result ) {
+				$response['installed'] = 'succeed';
 			} else {
-				wp_enqueue_script( 'plugin-install' );
-
-				$response['redirect'] = admin_url( '/themes.php?page=demo-importer&browse=all&spacious-hide-notice=welcome' );
-
-				/**
-				 * Install Plugin.
-				 */
-				include_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
-				include_once ABSPATH . 'wp-admin/includes/plugin-install.php';
-
-				$api = plugins_api( 'plugin_information', array(
-					'slug'   => sanitize_key( wp_unslash( 'themegrill-demo-importer' ) ),
-					'fields' => array(
-						'sections' => false,
-					),
-				) );
-
-				$skin     = new WP_Ajax_Upgrader_Skin();
-				$upgrader = new Plugin_Upgrader( $skin );
-				$result   = $upgrader->install( $api->download_link );
-				if ( $result ) {
-					$response['installed'] = 'succeed';
-				} else {
-					$response['installed'] = 'failed';
-				}
-
-				// Activate plugin.
-				if ( current_user_can( 'activate_plugin' ) ) {
-					$result = activate_plugin( 'themegrill-demo-importer/themegrill-demo-importer.php' );
-
-					if ( is_wp_error( $result ) ) {
-						$response['errorCode']    = $result->get_error_code();
-						$response['errorMessage'] = $result->get_error_message();
-					}
-				}
+				$response['installed'] = 'failed';
 			}
 
-			wp_send_json( $response );
+			// Activate plugin.
+			if ( current_user_can( 'activate_plugin' ) ) {
+				$result = activate_plugin( 'themegrill-demo-importer/themegrill-demo-importer.php' );
 
-			exit();
-
-		}
-
-		/**
-		 * Add admin notice.
-		 */
-		public function admin_notice() {
-			global $spacious_version, $pagenow;
-
-			wp_enqueue_style( 'spacious-message', get_template_directory_uri() . '/css/admin/message.css', array(), $spacious_version );
-
-			// Let's bail on theme activation.
-			$notice_nag = get_option( 'spacious_admin_notice_welcome' );
-			if ( ! $notice_nag ) {
-				add_action( 'admin_notices', array( $this, 'welcome_notice' ) );
-			}
-		}
-
-		/**
-		 * Hide a notice if the GET variable is set.
-		 */
-		public static function hide_notices() {
-			if ( isset( $_GET['spacious-hide-notice'] ) && isset( $_GET['_spacious_notice_nonce'] ) ) {
-				if ( ! wp_verify_nonce( $_GET['_spacious_notice_nonce'], 'spacious_hide_notices_nonce' ) ) {
-					wp_die( __( 'Action failed. Please refresh the page and retry.', 'spacious' ) );
-				}
-
-				if ( ! current_user_can( 'manage_options' ) ) {
-					wp_die( __( 'Cheatin&#8217; huh?', 'spacious' ) );
-				}
-
-				$hide_notice = sanitize_text_field( $_GET['spacious-hide-notice'] );
-				update_option( 'spacious_admin_notice_' . $hide_notice, 1 );
-
-				// Hide.
-				if ( 'welcome' === $_GET['spacious-hide-notice'] ) {
-					update_option( 'spacious_admin_notice_' . $hide_notice, 1 );
-				} else { // Show.
-					delete_option( 'spacious_admin_notice_' . $hide_notice );
+				if ( is_wp_error( $result ) ) {
+					$response['errorCode']    = $result->get_error_code();
+					$response['errorMessage'] = $result->get_error_message();
 				}
 			}
 		}
 
-		/**
-		 * Show welcome notice.
-		 */
-		public function welcome_notice() {
-			?>
-			<div id="message" class="updated spacious-message">
-				<a class="spacious-message-close notice-dismiss" href="<?php echo esc_url( wp_nonce_url( remove_query_arg( array( 'activated' ), add_query_arg( 'spacious-hide-notice', 'welcome' ) ), 'spacious_hide_notices_nonce', '_spacious_notice_nonce' ) ); ?>">
-					<?php esc_html_e( 'Dismiss', 'spacious' ); ?>
-				</a>
+		wp_send_json( $response );
 
-				<div class="spacious-message-wrapper">
-					<div class="spacious-logo">
-						<img src="<?php echo get_template_directory_uri(); ?>/img/spacious-getting-started-logo.png" alt="<?php esc_html_e( 'Spacious', 'spacious' ); ?>" /><?php // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped, Squiz.PHP.EmbeddedPhp.SpacingBeforeClose ?>
-					</div>
+		exit();
 
-					<p>
-						<?php printf( esc_html__( 'Welcome! Thank you for choosing Spacious! To fully take advantage of the best our theme can offer please make sure you visit our %swelcome page%s.', 'spacious' ), '<a href="' . esc_url( admin_url( 'themes.php?page=spacious-options' ) ) . '">', '</a>' ); ?>
+	}
 
-						<span class="plugin-install-notice"><?php esc_html_e( 'Clicking the button below will install and activate the ThemeGrill demo importer plugin.', 'spacious' ); ?></span>
-					</p>
+	/**
+	 * Add admin notice.
+	 */
+	public function admin_notice() {
+		wp_enqueue_style( 'spacious-message', get_template_directory_uri() . '/css/admin/message.css', array(), SPACIOUS_THEME_VERSION );
 
-					<div class="submit">
-						<a class="btn-get-started button button-primary button-hero" href="#" data-name="" data-slug="" aria-label="<?php esc_html_e( 'Get started with Spacious', 'spacious' ); ?>"><?php esc_html_e( 'Get started with Spacious', 'spacious' ); ?></a>
-					</div>
-				</div>
-			</div>
-			<?php
+		// Let's bail on theme activation.
+		$notice_nag = get_option( 'spacious_admin_notice_welcome' );
+		if ( ! $notice_nag ) {
+			add_action( 'admin_notices', array( $this, 'welcome_notice' ) );
 		}
 	}
 
-endif;
+	/**
+	 * Hide a notice if the GET variable is set.
+	 */
+	public static function hide_notices() {
 
-return new Spacious_Admin();
+		if ( isset( $_GET['spacious-hide-notice'] ) && isset( $_GET['_spacious_notice_nonce'] ) ) { // WPCS: input var ok.
+			if ( ! wp_verify_nonce( wp_unslash( $_GET['_spacious_notice_nonce'] ), 'spacious_hide_notices_nonce' ) ) { // phpcs:ignore WordPress.VIP.ValidatedSanitizedInput.InputNotSanitized
+				wp_die( __( 'Action failed. Please refresh the page and retry.', 'spacious' ) ); // WPCS: xss ok.
+			}
+
+			if ( ! current_user_can( 'manage_options' ) ) {
+				wp_die( __( 'Cheatin&#8217; huh?', 'spacious' ) ); // WPCS: xss ok.
+			}
+
+			$hide_notice = sanitize_text_field( wp_unslash( $_GET['spacious-hide-notice'] ) );
+			update_option( 'spacious_admin_notice_' . $hide_notice, 1 );
+
+			// Hide.
+			if ( 'welcome' === $_GET['spacious-hide-notice'] ) {
+				update_option( 'spacious_admin_notice_' . $hide_notice, 1 );
+			} else { // Show.
+				delete_option( 'spacious_admin_notice_' . $hide_notice );
+			}
+		}
+
+	}
+
+	/**
+	 * Return or echo `Get started/Import button` HTML.
+	 *
+	 * @param bool   $return    Return or echo.
+	 * @param string $slug      Plugin slug to install.
+	 * @param string $text      Text string for button.
+	 * @param string $css_class CSS class list for button link.
+	 *
+	 */
+	public static function import_button_html( $return = false, $slug = 'themegrill-demo-importer', $text, $css_class = 'btn-get-started button button-primary button-hero' ) {
+
+		if ( true === $return ) {
+			return '<a class="' . esc_attr( $css_class ) . '"
+		   href="#" data-name="' . esc_attr( $slug ) . '" data-slug="' . esc_attr( $slug ) . '" aria-label="' . esc_attr__( 'Get started with Spacious', 'spacious' ) . '">' . esc_html( $text ) . '</a>';
+		} else {
+			echo '<a class="' . esc_attr( $css_class ) . '"
+					href="#" data-name="' . esc_attr( $slug ) . '" data-slug="' . esc_attr( $slug ) . '" aria-label="' . esc_attr__( 'Get started with Spacious', 'spacious' ) . '">' . esc_html( $text ) . '</a>';
+		}
+
+	}
+
+	/**
+	 * Show welcome notice.
+	 */
+	public function welcome_notice() {
+		?>
+
+		<div id="message" class="updated notice-info spacious-message">
+			<a class="spacious-message-close notice-dismiss"
+			   href="<?php echo esc_url( wp_nonce_url( remove_query_arg( array( 'activated' ), add_query_arg( 'spacious-hide-notice', 'welcome' ) ), 'spacious_hide_notices_nonce', '_spacious_notice_nonce' ) ); ?>">
+				<?php esc_html_e( 'Dismiss', 'spacious' ); ?>
+			</a>
+			<div class="spacious-message-wrapper">
+				<img class="spacious-screenshot" src="<?php echo get_template_directory_uri(); ?>/screenshot.jpg" alt="<?php esc_html_e( 'Spacious', 'spacious' ); ?>" /><?php // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped, Squiz.PHP.EmbeddedPhp.SpacingBeforeClose ?>
+
+				<div class="spacious-getting-started-notice">
+					<h2>
+						<?php
+						printf(
+						/* translators: 1: welcome page link starting html tag, 2: welcome page link ending html tag. */
+							esc_html__( 'Welcome! Thank you for choosing Spacious! To fully take advantage of the best our theme can offer, please make sure you visit our %1$swelcome page%2$s.', 'spacious' ), '<a href="' . esc_url( admin_url( 'themes.php?page=spacious-options' ) ) . '">', '</a>' );
+						?>
+					</h2>
+
+					<p class="plugin-install-notice"><?php esc_html_e( 'Clicking the button below will install and activate the ThemeGrill demo importer plugin.', 'spacious' ); ?></p>
+
+					<?php self::import_button_html( false, '', esc_html__( 'Get started with Spacious', 'spacious' ) ); ?>
+				</div>
+			</div>
+		</div>
+		<?php
+	}
+
+}
+
+$admin = new Spacious_Admin();
