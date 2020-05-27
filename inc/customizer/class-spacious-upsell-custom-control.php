be9d930e2f37a8f96c18b4f374a877a2257c41f0
ThemeGrill <milanthapa299@gmail.com>
2020-05-27 09:03:54 +0545

    Updated Upsell section text domain and code

--- inc/customizer/class-spacious-upsell-custom-control.php
+++ inc/customizer/class-spacious-upsell-custom-control.php
@@ -10,26 +10,29 @@ class Spacious_Upsell_Custom_Control extends WP_Customize_Control {
 
 	public function render_content() {
 		?>
-		<div class="zakra-upsell-wrapper">
+		<div class="spacious-upsell-wrapper">
 			<ul class="upsell-features">
-				<h3 class="upsell-heading"><?php esc_html_e( 'More Awesome Features', 'zakra' ); ?></h3>
+				<h3 class="upsell-heading"><?php esc_html_e( 'More Awesome Features', 'spacious' ); ?></h3>
 				<li class="upsell-feature"><span
-						class="dashicons dashicons-yes"></span><?php esc_html_e( 'Advanced Header Options', 'zakra' ); ?>
+						class="dashicons dashicons-yes"></span><?php esc_html_e( 'Advanced Header Options', 'spacious' ); ?>
 				</li>
 				<li class="upsell-feature"><span
-						class="dashicons dashicons-yes"></span><?php esc_html_e( 'Flexible Menu Designs', 'zakra' ); ?>
+						class="dashicons dashicons-yes"></span><?php esc_html_e( 'Flexible Menu Designs', 'spacious' ); ?>
 				</li>
 				<li class="upsell-feature"><span
-						class="dashicons dashicons-yes"></span><?php esc_html_e( 'Grid, Masonry, Thumbnail Blog', 'zakra' ); ?>
+						class="dashicons dashicons-yes"></span><?php esc_html_e( 'Grid, Masonry, Thumbnail Blog', 'spacious' ); ?>
 				</li>
 				<li class="upsell-feature"><span
-						class="dashicons dashicons-yes"></span><?php esc_html_e( '10+ Footer Layouts', 'zakra' ); ?>
+						class="dashicons dashicons-yes"></span><?php esc_html_e( '10+ Footer Layouts', 'spacious' ); ?>
 				</li>
 				<li class="upsell-feature"><span
-						class="dashicons dashicons-yes"></span><?php esc_html_e( '100+ Customizer Options', 'zakra' ); ?>
+						class="dashicons dashicons-yes"></span><?php esc_html_e( '100+ Customizer Options', 'spacious' ); ?>
 				</li>
 				<li class="upsell-feature"><span
-						class="dashicons dashicons-yes"></span><?php esc_html_e( '30+ Page Settings', 'zakra' ); ?>
+						class="dashicons dashicons-yes"></span><?php esc_html_e( 'Advanced Page Settings', 'spacious' ); ?>
+				</li>
+				<li class="upsell-feature"><span
+						class="dashicons dashicons-yes"></span><?php esc_html_e( '17+ Starter Demos', 'spacious' ); ?>
 				</li>
 			</ul>
 
@@ -37,13 +40,16 @@ class Spacious_Upsell_Custom_Control extends WP_Customize_Control {
 				<?php
 				printf(
 				/* translators: %1$s discount coupon code., %2$s discount percentage */
-					esc_html__( 'Use the coupon code %1$s to get %2$s discount (limited time offer). Enjoy!', 'zakra' ),
+					esc_html__( 'Use the coupon code %1$s to get %2$s discount (limited time offer). Enjoy!', 'spacious' ),
 					'<span class="coupon-code">save10</span>',
 					'10%'
 				);
 				?>
 			</div>
-		</div>
+		</div> <!-- /.spacious-upsell-wrapper -->
+
+		<a class="upsell-cta" target="_blank"
+		   href="<?php echo esc_url( 'https://themegrill.com/spacious-pricing/?utm_source=spacious-customizer&utm_medium=view-pricing-link&utm_campaign=upgrade' ); ?>"><?php esc_html_e( 'View Pricing', 'spacious' ); ?></a>
 		<?php
 	}
 
